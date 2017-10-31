3a46252891c069e6877c36e70baf3c560ea0147f
J. Alexander <xandermoorman@gmail.com>
2017-10-31 01:30:24 -0500

    Updated Program Significantly

--- Invoke-SecurityCheck.ps1
+++ Invoke-SecurityCheck.ps1
@@ -35,11 +35,11 @@ CheckRunKey
 # NOTE: I made this list very quickly. 
 CheckSecurityEvts
 # returns array with critical and error events from last 24 hours System, Security and Applcation event logs
-Last24HoursCritical
+Last24Hours
 # Get all critical and error events in System, Security and Application event logs
-Get-WinEvent -FilterHashtable @{LogName = "System"; Level = 1,2} | Export-Csv -Path "C:\Users\wobblywudude\Documents\criticalsyslog.csv" -Delimiter '|'
-Get-WinEvent -FilterHashtable @{LogName = "Security"; Level = 1,2} | Export-Csv -Path "C:\Users\wobblywudude\Documents\criticalseclog.csv" -Delimiter '|'
-Get-WinEvent -FilterHashtable @{LogName = "Application"; Level = 1,2} | Export-Csv -Path "C:\Users\wobblywudude\Documents\criticalapplog.csv" -Delimiter '|'
+Get-EventLog Security | Export-Csv -Path "C:\Users\wobblywudude\Documents\securitylog.csv" -Delimiter '|'
+Get-EventLog Application | Export-Csv -Path "C:\Users\wobblywudude\Documents\applog.csv" -Delimiter '|'
+Get-EventLog System | Export-Csv -Path "C:\Users\wobblywudude\Documents\systemlog.csv" -Delimiter '|'
 
 # Grab Scheduled Tasks
 # schtasks | Out-File -FilePath "C:\Users\wobblywudude\Documents\schtasks.txt"
@@ -90,8 +90,6 @@ $concat | Out-File -FilePath "C:\Users\wobblywudude\Documents\processdetails.txt
 
 function AutostartPrograms{
 wmic startup list full | Out-File -FilePath "C:\Users\wobblywudude\Documents\autostartprograms.txt"
-
-
 }
 
 # Check Run Keys
@@ -189,11 +187,11 @@ $uptime = $counter.CounterSample[0].CookedValue
 New-TimeSpan -Seconds $uptime | Out-File -FilePath "C:\Users\wobblywudude\Documents\uptime.txt"
 }
 
-function Last24HoursCritical{
+function Last24Hours{
 $compareDate = (Get-Date).AddDays(-1)
-$app = Get-WinEvent -FilterHashtable @{LogName = "Application"; Level = 1,2} | Where-Object {$_.Time -lt $compareDate} | Export-Csv -Path "C:\Users\wobblywudude\Documents\critical24hrlog.csv" -Delimiter '|'
-$sys = Get-WinEvent -FilterHashtable @{LogName = "System"; Level = 1,2} | Where-Object {$_.Time -lt $compareDate} | Select-Object -ExpandProperty | Export-Csv -Append "C:\Users\wobblywudude\Documents\critical24hrlog.csv" -Delimiter '|'
-$sec = Get-WinEvent -FilterHashtable @{LogName = "Security"; Level = 1,2} | Where-Object {$_.Time -lt $compareDate} | Select-Object -ExpandProperty | Export-Csv -Append "C:\Users\wobblywudude\Documents\critical24hrlog.csv" -Delimiter '|'
+Get-EventLog Application | Where-Object {$_.Time -lt $compareDate} | Export-Csv -Path "C:\Users\wobblywudude\Documents\critical24hrlog.csv" -Delimiter '|'
+Get-EventLog Security | Where-Object {$_.Time -lt $compareDate} | Select-Object -ExpandProperty | Export-Csv -Append "C:\Users\wobblywudude\Documents\critical24hrlog.csv" -Delimiter '|'
+Get-EventLog System | Where-Object {$_.Time -lt $compareDate} | Select-Object -ExpandProperty | Export-Csv -Append "C:\Users\wobblywudude\Documents\critical24hrlog.csv" -Delimiter '|'
 #$arr = New-Object System.Collections.ArrayList 
 #[void] $arr.Add($app)
 #[void] $arr.Add($sys)
@@ -205,7 +203,7 @@ $sec = Get-WinEvent -FilterHashtable @{LogName = "Security"; Level = 1,2} | Wher
 function CheckSecurityEvts{
 [regex] $securityIdArr = "1100|1102|1108|4616|4618|4625|4649|4650|4651|4652|4653|4654|4655|4656|4657|4659|4660|4663|4670|4671|4672|4688|4690|4691|4692|4693|4697|4698|4699|4701|4702|4703|4704|4705|4706|4709|4710|4712|4713|4714|4715|4717|4716|4718|4719|4720|4722|4725|4726|4727|4728|4732|4738|4740|4741|4742|4744|4745|4746|4756|4764|4767|4771|4772|4774|4775|4777|4780|4781|4782|4790|4794|4797|4798|4816|4819|4820|4821|4822|4823|4824|4825|4830|4864|4868|4869|4870|4871|4873|4882|4884|4885|4887|4888|4895|4896|4946|4947|4948|4949|4950|4951|4952|4953|4954|4957|4958|4960|4961|4962|4963|4964|4965|4976|4977|4978|4979|4980|4981|4982|4983|4984|5024|5025|5026|5027|5028|5029|5030|5031|5032|5033|5034|5035|5037|5038|5040|5041|5042|5043|5044|5045|5046|5047|5048|5049|5050|5057|5071|5120|5121|5122|5123|5124|5126|5143|5137|5145|5148|5151|5150|5152|5155|5156|5157|5158|5159|5168|5376|5377|5378|5451|5452|5453|5456|5457|5478|5479|5480|5483|5484|5485|6144|6145|6273|6276|6277|6279|6281|6406|6418|6423|6423" # END populate security ID array
 
-Get-WinEvent @{logname="security";id=$id} | Where-Object {$_ -match $securityIdArr}  | Export-Csv -Path "C:\Users\wobblywudude\Documents\dangerSecLog.csv" -Delimiter '|'
+Get-EventLog Security | Where-Object {$_ -match $securityIdArr}  | Export-Csv -Path "C:\Users\wobblywudude\Documents\dangerSecLog.csv" -Delimiter '|'
 
 # Grab all logins. We can use python program to check logon types later. 
 # Look for login types 3,4,8,9,10,11. Remove Types 2 and 7 
